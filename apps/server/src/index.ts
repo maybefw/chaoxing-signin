4402c989cb68bd9ee0c6f288395738c5db021ebb
actually <jile4149@gmail.com>
2024-11-11 22:34:39 +0800

    Update index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,4 @@
+//这个文件就是最根本的文件 调用登陆函数，各个签到文件中的签到函数，还有可以从本地用户调用
 import prompts from 'prompts';/你好
 import { blue, red } from 'kolorist';
 import { getPPTActiveInfo, preSign, traverseCourseActivity } from './functions/activity';
@@ -13,12 +14,13 @@ const PromptsOptions = {
     console.log(red('✖') + ' 操作取消');
     process.exit(0);
   },
-};
+};//是一个可选项 由用户决定当前操作，在取消操作时输出一个红色的提示消息，然后正常退出程序。
 
 (async function () {
   let params: UserCookieType & { phone?: string; };
   const configs: any = {};
   // 本地与登录之间的抉择
+  // 本地与登录之间的抉择，调用 getLocalUsers方法查看是否有本地用户。如有可以选择用户登录
   {
     // 打印本地用户列表，并返回用户数量
     const { userItem } = await prompts({
@@ -33,14 +35,16 @@ const PromptsOptions = {
       const { phone } = await prompts({ type: 'text', name: 'phone', message: '手机号' }, PromptsOptions);
       const { password } = await prompts({ type: 'password', name: 'password', message: '密码' }, PromptsOptions);
       // 登录获取各参数
-      const result = await userLogin(phone, password);
+      const result = await userLogin(phone, password);//调用 userLogin函数获取用户参数，储存在result
       if (typeof result === 'string') process.exit(0);
-      else storeUser(phone, { phone, params: result }); // 储存到本地
-      params = { ...result, phone };
+      else storeUser(phone, { phone, params: result }); //// 把result参数储存到本地，改名为params储存（注意这是一个立即调用函数，定义的变量是全局变量）
+      params = { ...result, phone };//改名为params储存
+      console.log(params)
     } else {
-      // 使用本地储存的参数
-      const jsonObject = getJsonObject('configs/storage.json').users[userItem];
+      // 使用本地储存的参数（老用户）
+      const jsonObject = getJsonObject('configs/storage.json').users[userItem];//getJsonObject方法转化storage.json中的信息为一个Object
       params = { ...jsonObject.params };
+       console.log(params)
       params.phone = jsonObject.phone;
       configs.monitor = { ...jsonObject.monitor };
       configs.mailing = { ...jsonObject.mailing };
@@ -50,23 +54,24 @@ const PromptsOptions = {
   }
 
   // 获取用户名
-  const name = await getAccountInfo(params);
-  console.log(blue(`你好，${name}`));
+  const name = await getAccountInfo(params);//params的定义在上面哈，分为新用户和老用户
+  console.log(blue(`你好，${name}`));//blue就是把${name}转化为蓝色字体
 
   // 获取所有课程
-  const courses = await getCourses(params._uid, params._d, params.vc3);
-  if (typeof courses === 'string') process.exit(0);
+  const courses = await getCourses(params._uid, params._d, params.vc3);//params的定义在上面哈，分为新用户和老用户
+  if (typeof courses === 'string') process.exit(0);//getCourses函数返回一个courses列表 里面是courseId和classId
   // 获取进行中的签到活动
   const activity = await traverseCourseActivity({ courses, ...params });
-  if (typeof activity === 'string') process.exit(0);
-  else await preSign({ ...activity, ...params });
+  if (typeof activity === 'string') process.exit(0);//如果没有签到活动就退出
+  else await preSign({ ...activity, ...params });//如果有签到活动就执行预签
 
   // 处理签到，先进行预签
   switch (activity.otherId) {
     case 2: {
       // 二维码签到
+      //otherId是可以用来判断签到类型
       const { enc } = await prompts({ type: 'text', name: 'enc', message: 'enc(微信或其他识别二维码，可得enc参数)' }, PromptsOptions);
-      const defaultLngLat = configs.monitor?.lon ? `${configs.monitor.lon},${configs.monitor.lat}` : '113.516288,34.817038';
+      const defaultLngLat = configs.monitor?.lon ? `${configs.monitor.lon},${configs.monitor.lat}` : '113.516288,34.817038';//经纬度选择
       const defaultAddress = configs.monitor?.address ? configs.monitor.address : '';
       const { lnglat } = await prompts({ type: 'text', name: 'lnglat', message: '经纬度', initial: defaultLngLat }, PromptsOptions);
       const { address } = await prompts({ type: 'text', name: 'address', message: '详细地址', initial: defaultAddress });
